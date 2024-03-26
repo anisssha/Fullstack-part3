ffaedd83b1bdbcc07ccaaaf425ff7915eaf67780
anisha stha <stha.anisha16@gmail.com>
2024-03-27 00:19:54 +0545

    changes in port in index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post("/api/persons", (req, res) => {
     res.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () =>
     console.log(`listening on port http://localhost:${PORT}`)
